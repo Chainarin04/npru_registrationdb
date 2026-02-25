c7f51c3a86878cfd277d6e094ae1e90402dd1297
Chainarin Phudphai <684259042@webmail.npru.ac.th>
2026-02-25 15:45:19 +0700

    Add files via upload

---
+++ npru_registrationdb.sql
@@ -0,0 +1,343 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2026 at 09:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `npru_registrationdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `title`, `message`, `created_at`) VALUES
+(1, 'เทสๆ', 'เบื่อออออออ', '2026-02-25 04:00:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` varchar(10) NOT NULL,
+  `course_name_en` varchar(100) NOT NULL,
+  `course_name_th` varchar(100) NOT NULL,
+  `credits` int(11) NOT NULL,
+  `program_id` varchar(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course_id`, `course_name_en`, `course_name_th`, `credits`, `program_id`) VALUES
+('7151101', 'Introduction to Algorithm', 'อัลกอริทึมเบื้องต้น', 3, '715'),
+('7152201', 'Database System', 'ระบบฐานข้อมูล', 3, '715'),
+('7152214', 'Introduction to Software Engineering', 'วิศวกรรมซอฟต์แวร์เบื้องต้น', 3, '715'),
+('7153301', 'Database Management', 'การบริหารจัดการฐานข้อมูล', 3, '715'),
+('CS101', '', 'คอมพิวเตอร์เบื้องต้น', 3, NULL),
+('CS102', '', 'การเขียนโปรแกรมพื้นฐาน', 3, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE `programs` (
+  `program_id` varchar(3) NOT NULL,
+  `program_name_th` varchar(100) NOT NULL,
+  `program_name_en` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `programs`
+--
+
+INSERT INTO `programs` (`program_id`, `program_name_th`, `program_name_en`) VALUES
+('715', 'วิศวกรรมซอฟต์แวร์', 'Software Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrations`
+--
+
+CREATE TABLE `registrations` (
+  `reg_id` int(11) NOT NULL,
+  `student_id` varchar(15) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `academic_year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `registrations`
+--
+
+INSERT INTO `registrations` (`reg_id`, `student_id`, `semester`, `academic_year`) VALUES
+(1, '664143001', 1, 2566),
+(2, '664143001', 1, 2567);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration_details`
+--
+
+CREATE TABLE `registration_details` (
+  `detail_id` int(11) NOT NULL,
+  `reg_id` int(11) NOT NULL,
+  `course_id` varchar(10) NOT NULL,
+  `grade` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `registration_details`
+--
+
+INSERT INTO `registration_details` (`detail_id`, `reg_id`, `course_id`, `grade`) VALUES
+(1, 1, '7151101', 'B+'),
+(2, 1, '7152214', 'B'),
+(3, 1, '7152201', 'C+'),
+(4, 1, '7153301', 'A'),
+(5, 2, '7151101', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` varchar(15) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `program_id` varchar(3) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `profile_pic` varchar(255) DEFAULT 'default.png',
+  `phone` varchar(15) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `program_id`, `dob`, `profile_pic`, `phone`, `address`, `email`) VALUES
+('664143001', 'ชัยณรินทร์', 'ผุดผาย', '715', '2004-05-24', 'default.png', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study_plan`
+--
+
+CREATE TABLE `study_plan` (
+  `plan_id` int(11) NOT NULL,
+  `program_id` int(11) DEFAULT NULL,
+  `course_id` varchar(10) DEFAULT NULL,
+  `semester` int(11) DEFAULT NULL,
+  `academic_year` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `study_plan`
+--
+
+INSERT INTO `study_plan` (`plan_id`, `program_id`, `course_id`, `semester`, `academic_year`) VALUES
+(3, 1, 'CS101', 1, 2567),
+(4, 1, 'CS102', 1, 2567),
+(5, 1, 'CS101', 1, 2567),
+(6, 1, 'CS102', 1, 2567);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `student_id` varchar(15) DEFAULT NULL,
+  `role` enum('student','admin') DEFAULT 'student'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `student_id`, `role`) VALUES
+(1, 'somsak', '1234', '664143001', 'student'),
+(2, 'teacher', '1234', NULL, 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `program_id` (`program_id`);
+
+--
+-- Indexes for table `programs`
+--
+ALTER TABLE `programs`
+  ADD PRIMARY KEY (`program_id`);
+
+--
+-- Indexes for table `registrations`
+--
+ALTER TABLE `registrations`
+  ADD PRIMARY KEY (`reg_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `registration_details`
+--
+ALTER TABLE `registration_details`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `reg_id` (`reg_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `program_id` (`program_id`);
+
+--
+-- Indexes for table `study_plan`
+--
+ALTER TABLE `study_plan`
+  ADD PRIMARY KEY (`plan_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `registrations`
+--
+ALTER TABLE `registrations`
+  MODIFY `reg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `registration_details`
+--
+ALTER TABLE `registration_details`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `study_plan`
+--
+ALTER TABLE `study_plan`
+  MODIFY `plan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`program_id`);
+
+--
+-- Constraints for table `registrations`
+--
+ALTER TABLE `registrations`
+  ADD CONSTRAINT `registrations_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+
+--
+-- Constraints for table `registration_details`
+--
+ALTER TABLE `registration_details`
+  ADD CONSTRAINT `registration_details_ibfk_1` FOREIGN KEY (`reg_id`) REFERENCES `registrations` (`reg_id`),
+  ADD CONSTRAINT `registration_details_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`program_id`);
+
+--
+-- Constraints for table `study_plan`
+--
+ALTER TABLE `study_plan`
+  ADD CONSTRAINT `study_plan_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
